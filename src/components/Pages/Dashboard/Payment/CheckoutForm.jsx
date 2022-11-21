c4322e352fea64fbdde3bed81bd83908163c5671
mehedititu <mh.me.mehedi@gmail.com>
2022-11-21 23:10:44 +0600

    Checkout page done

--- src/components/Pages/Dashboard/Payment/CheckoutForm.jsx
+++ src/components/Pages/Dashboard/Payment/CheckoutForm.jsx
@@ -1,13 +1,29 @@
 import { CardElement, useElements, useStripe } from '@stripe/react-stripe-js';
-import React, {  useState } from 'react';
+import React, { useEffect, useState } from 'react';
 
-const CheckoutForm = ({payments}) => {
+const CheckoutForm = ({ payments }) => {
+    const [clientSecret, setClientSecret] = useState("");
     const [cardError, setCardError] = useState('');
+    const [paymentSuccess, setPaymentSuccess] = useState('')
+    const [paymentId, setPaymentId] = useState('');
+    const [paymentLoadding, setPaymentLoadding] = useState(false);
     const stripe = useStripe();
     const elements = useElements();
-    const {price} = payments;
+    const { price, patient, email } = payments;
 
-    
+    useEffect(() => {
+        // Create PaymentIntent as soon as the page loads
+        fetch("http://localhost:5000/create-payment-intent", {
+            method: "POST",
+            headers: {
+                "Content-Type": "application/json",
+                authorization: `bearar ${localStorage.getItem('appointmentToken')}`
+            },
+            body: JSON.stringify({ price }),
+        })
+            .then((res) => res.json())
+            .then((data) => setClientSecret(data.clientSecret));
+    }, [price]);
 
     const handleSubmit = async (event) => {
         event.preventDefault();
@@ -32,6 +48,30 @@ const CheckoutForm = ({payments}) => {
         } else {
             setCardError('')
         }
+
+        setPaymentSuccess('');
+        setPaymentLoadding(true)
+        const {paymentIntent, paymentError} = await stripe.confirmCardPayment(
+            clientSecret,
+            {
+              payment_method: {
+                card: card,
+                billing_details: {
+                  name: patient,
+                  email: email,
+                },
+              },
+            },
+        );
+        if(paymentError) {
+            setCardError(paymentError.message);
+            return
+        }
+        if(paymentIntent.status === "succeeded" ) {
+            setPaymentSuccess('You have successfully perchess');
+            setPaymentId(`Your Payment id is ${paymentIntent.id}`)
+        }
+        setPaymentLoadding(false)
     };
     return (
         <form onSubmit={handleSubmit}>
@@ -51,12 +91,18 @@ const CheckoutForm = ({payments}) => {
                     },
                 }}
             />
+            <button className='btn btn-primary btn-sm mt-3' type="submit" disabled={!stripe || !clientSecret || paymentLoadding}>
+                Pay
+            </button>
             {
                 cardError && <p className="text-red-500 mt-2">{cardError}</p>
             }
-            <button className='btn btn-primary btn-sm mt-3' type="submit" disabled={!stripe }>
-                Pay
-            </button>
+            {
+                paymentSuccess && <div className='text-green-500 mt-2 font-semibold'>
+                    <p>{paymentSuccess}</p>
+                    <p>{paymentId}</p>
+                </div>
+            }
         </form>
     );
 };
